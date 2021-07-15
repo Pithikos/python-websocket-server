72472be48454cf159221d84fc323b5d6acb4cf06
Mano <manossef@gmail.com>
2021-07-15 10:48:52 +0100

    Add test_server.py

---
+++ tests/test_server.py
@@ -0,0 +1,17 @@
+from utils import session, server
+
+import pytest
+
+
+def test_client_closes_gracefully(session):
+    client, server = session
+    assert client.connected
+    assert server.clients
+    old_client_handler = server.clients[0]["handler"]
+    client.close()
+    assert not client.connected
+
+    # Ensure server closed connection
+    assert not server.clients
+    with pytest.raises(BrokenPipeError):
+        old_client_handler.connection.send(b"test")
