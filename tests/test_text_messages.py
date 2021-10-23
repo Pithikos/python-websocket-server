bcef2715bd9ea220a19d241ef47db0a867bbd48b
Mano <manossef@gmail.com>
2021-10-23 11:17:33 +0100

    Refactor test_text_messages to use existing fixtures

--- tests/test_text_messages.py
+++ tests/test_text_messages.py
@@ -1,89 +1,89 @@
-from utils import session, server
+from utils import session, threaded_server
 
 
 def test_text_message_of_length_1(session):
-    client, server = session
+    conn, server = session
     server.send_message_to_all('$')
-    assert client.recv() == '$'
+    assert conn.recv() == '$'
 
 
 def test_text_message_of_length_125B(session):
-    client, server = session
+    conn, server = session
     msg = 'abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz'\
           'abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz'\
           'abcdefghijklmnopqr125'
     server.send_message_to_all(msg)
-    assert client.recv() == msg
+    assert conn.recv() == msg
 
 
 def test_text_message_of_length_126B(session):
-    client, server = session
+    conn, server = session
     msg = 'abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz'\
           'abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz'\
           'abcdefghijklmnopqrs126'
     server.send_message_to_all(msg)
-    assert client.recv() == msg
+    assert conn.recv() == msg
 
 
 def test_text_message_of_length_127B(session):
-    client, server = session
+    conn, server = session
     msg = 'abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz'\
           'abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz'\
           'abcdefghijklmnopqrst127'
     server.send_message_to_all(msg)
-    assert client.recv() == msg
+    assert conn.recv() == msg
 
 
 def test_text_message_of_length_208B(session):
-    client, server = session
+    conn, server = session
     msg = 'abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz'\
           'abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz'\
           'abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz'\
           'abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvw208'
     server.send_message_to_all(msg)
-    assert client.recv() == msg
+    assert conn.recv() == msg
 
 
 def test_text_message_of_length_1251B(session):
-    client, server = session
+    conn, server = session
     msg = ('abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz'\
           'abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz'\
           'abcdefghijklmnopqr125'*10)+'1'
     server.send_message_to_all(msg)
-    assert client.recv() == msg
+    assert conn.recv() == msg
 
 
 def test_text_message_of_length_68KB(session):
-    client, server = session
+    conn, server = session
     msg = '$'+('a'*67993)+'68000'+'^'
     assert len(msg) == 68000
     server.send_message_to_all(msg)
-    assert client.recv() == msg
+    assert conn.recv() == msg
 
 
 def test_text_message_of_length_1500KB(session):
     """ An enormous message (well beyond 65K) """
-    client, server = session
+    conn, server = session
     msg = '$'+('a'*1499991)+'1500000'+'^'
     assert len(msg) == 1500000
     server.send_message_to_all(msg)
-    assert client.recv() == msg
+    assert conn.recv() == msg
 
 
 def test_text_message_with_unicode_characters(session):
-    client, server = session
+    conn, server = session
     msg = '$äüö^'
     server.send_message_to_all(msg)
-    assert client.recv() == msg
+    assert conn.recv() == msg
 
 
 def test_text_message_stress_bursts(session):
-    """ Scenario: server sends multiple different message to the same client
+    """ Scenario: server sends multiple different message to the same conn
     at once """
     from threading import Thread
     NUM_THREADS = 100
     MESSAGE_LEN = 1000
-    client, server = session
+    conn, server = session
     messages_received = []
 
     # Threads receing
@@ -91,7 +91,7 @@ def test_text_message_stress_bursts(session):
     for i in range(NUM_THREADS):
         th = Thread(
             target=lambda fn: messages_received.append(fn()),
-            args=(client.recv,)
+            args=(conn.recv,)
         )
         th.daemon = True
         threads_receiving.append(th)
