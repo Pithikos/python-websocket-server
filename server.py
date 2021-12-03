530c0ebf270485a3188007023fea2fbc7c914e7c
Zhu S.R <67990189+MuRongPIG@users.noreply.github.com>
2021-12-03 21:10:38 +0800

    Update server.py

--- server.py
+++ server.py
@@ -19,7 +19,7 @@ def message_received(client, server, message):
 
 
 PORT=9001
-server = WebsocketServer(PORT)
+server = WebsocketServer(port = PORT)
 server.set_fn_new_client(new_client)
 server.set_fn_client_left(client_left)
 server.set_fn_message_received(message_received)
